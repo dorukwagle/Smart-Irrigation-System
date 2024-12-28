3889c28598eaf3b6b1c0f6046b1802482f7bf05d
dorukwagle <dorukwagle@gmail.com>
2024-12-28 12:34:34 +0545

    add network error dialog

--- client/src/entities/constants.ts
+++ client/src/entities/constants.ts
@@ -1,6 +1,7 @@
 const BASE_URL = "http://localhost:8080/api";
 const USER_CACHE_KEY = ["user"];
 const TODOS_CACHE_KEY = ["todos"];
+const NET_ERR_KEY = ["net_err"];
 
 const HOUR = 60 * 60 * 1000;
 const DAY = 24 * HOUR;
@@ -9,6 +10,7 @@ export {
     USER_CACHE_KEY,
     TODOS_CACHE_KEY,
     BASE_URL,
+    NET_ERR_KEY,
     HOUR,
     DAY
 }
