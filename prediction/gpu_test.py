ddf891d4e953b3687a8bbc46413d186762fbaac8
dorukwagle <dorukwagle@gmail.com>
2025-01-05 19:52:12 +0545

    train the model

---
+++ prediction/gpu_test.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    print(f"TensorFlow is using GPU: {physical_devices[0]}")
+else:
+    print("No GPU devices found. TensorFlow will run on CPU.")
